978a491e85d8e768c42c363c56618b771352491e
Václav Volhejn <vaclav.volhejn@gmail.com>
2019-10-08 23:05:42 +0200

    Add penultimate

--- penultimate.c
+++ penultimate.c
@@ -1,16 +1,24 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
 #include <sys/types.h>
 
 int main() {
-    ssize_t nread;
-    size_t len = 0;
-    char* line = NULL;
+  ssize_t nread;
+  size_t len = 0;
+  char* lines[2];
 
-    while ((nread = getline(&line, &len, stdin)) != -1) {
-        printf("Retrieved line of length %zu:\n", nread);
-        fwrite(line, nread, 1, stdout);
-    }
-    free(line);
+  for (int i = 0; i < 2; i++) {
+    lines[i] = malloc(1024 * sizeof(*lines));
+  }
+
+  int cur = 1;
+  while ((nread = getline(&lines[cur], &len, stdin)) != -1) {
+    cur = (cur + 1) % 2;
+  }
+  printf("%s", lines[cur]);
+
+  for (int i = 0; i < 2; i++) {
+    free(lines[i]);
+  }
 }
