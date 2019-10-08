26b7a0b40d578fb348222600778481c4c5b9cca0
Václav Volhejn <vaclav.volhejn@gmail.com>
2019-10-08 21:30:17 +0200

    Use clang-format to format code

--- endianness.c
+++ endianness.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
 #include <assert.h>
+#include <stdio.h>
 
 int main() {
-    short x = 1;
-    assert(sizeof(short)==2);
-    char c0 = *(&x);
-    char c1 = *(&x+1);
-    if (c0 == 0 && c1 == 1) {
-        printf("big endian\n");
-    } else if (c0 == 1 && c1 == 0) {
-        printf("little endian\n");
-    } else {
-        printf("something is amiss\n");
-    }
+  short x = 1;
+  assert(sizeof(short) == 2);
+  char c0 = *(&x);
+  char c1 = *(&x + 1);
+  if (c0 == 0 && c1 == 1) {
+    printf("big endian\n");
+  } else if (c0 == 1 && c1 == 0) {
+    printf("little endian\n");
+  } else {
+    printf("something is amiss\n");
+  }
 }
