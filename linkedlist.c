7f001911ae9ed97d5315b2f26abbbc5dcc2ee56c
Václav Volhejn <vaclav.volhejn@gmail.com>
2019-10-08 21:11:31 +0200

    Add linked list task

---
+++ linkedlist.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct List List;
+
+struct List {
+    char* val;
+    List* next;
+};
+
+int main(int argc, char** argv) {
+    List l = {NULL, NULL};
+    List* cur = &l;
+    for (int i = 0; i < argc; i++) {
+        cur->val = argv[i];
+        if (i < argc - 1) {
+            cur->next = malloc(sizeof(List));
+            cur = cur->next;
+        }
+    }
+
+    cur = &l;
+
+    while (cur != NULL) {
+        printf("%s\n", cur->val);
+        List* to_free = cur;
+        cur = cur->next;
+        if (to_free != &l) {
+            free(to_free);
+        }
+    }
+}
